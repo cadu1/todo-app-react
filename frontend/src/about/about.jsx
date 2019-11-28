aafedbabe86f09bb4b312a5190202cd55e654b83
Carlos <carlos@upoutsourcing.com>
2019-11-28 11:18:19 -0300

    Add form component and List component

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import PageHeader from '../template/header'
+
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name="Sobre" small='Nós' />
+        <h2>Nossa Missão</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec feugiat elementum dapibus. Nullam fringilla consequat ipsum, ac consequat ligula interdum a. Phasellus commodo, sem eget dapibus euismod, ligula metus porta odio, at tempus risus tortor a tortor. Nam vulputate ex nibh, vel consequat ipsum porta id. Donec et mollis turpis. Integer maximus luctus ligula quis dapibus. Sed commodo luctus ex sed dapibus. Vivamus velit ligula, ullamcorper in ex at, scelerisque accumsan dui.</p>
+        <h2>Nossa Missão e Visão</h2>
+        <p>Maecenas vulputate cursus volutpat. Donec facilisis, dolor eget interdum congue, neque orci blandit mauris, eu auctor libero odio et ex. Quisque quis lorem hendrerit, sagittis odio non, finibus sem. Integer porta ligula eleifend, gravida est vitae, varius lorem. Donec tincidunt diam arcu, id sollicitudin justo consequat et. Proin eu facilisis eros. Aliquam neque arcu, semper aliquam neque eu, volutpat interdum ex. Integer bibendum magna eget congue scelerisque. Aenean varius elit molestie massa fringilla condimentum. Phasellus turpis elit, scelerisque id gravida eu, ultricies eu libero.</p>
+        <p>Cras pellentesque mauris quis velit accumsan, a sollicitudin massa laoreet. Morbi in ipsum ac urna maximus interdum. Nullam at est vel elit molestie malesuada. Vivamus venenatis mauris ipsum, vitae dictum nulla porta et. In erat ipsum, maximus eu tincidunt at, vehicula eu diam. Nullam lacinia quis libero sed consequat. Nullam at malesuada nisi, tempus dapibus turpis. Aliquam urna purus, ultricies quis eleifend facilisis, dictum sit amet dui. Quisque bibendum scelerisque scelerisque. Vivamus pretium odio non nulla ornare tempus. Morbi diam lectus, molestie sed vehicula vitae, vulputate eu ex.</p>
+        <h2>Imprenssa</h2>
+        <p>Pellentesque sollicitudin neque sed leo euismod maximus quis et leo. Morbi tincidunt ultricies tellus. Aenean maximus nisl mauris, vitae accumsan diam lacinia eget. Mauris lectus lorem, ultrices nec egestas sit amet, viverra ut nibh. Phasellus sit amet ante non leo sagittis sodales. Ut vel iaculis erat. Donec et volutpat magna. Nam commodo ligula sit amet mauris semper, eget volutpat nunc ultricies. Nullam ac euismod libero. Donec ut feugiat felis.</p>
     </div>
 )
--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
+import PageHeader from '../template/header'
+import Form from './todoForm'
+import List from './todoList'
+
 export default class Todo extends Component {
     render() {
         return (
             <div>
-                <h1>Todo</h1>
+                <PageHeader name='Tarefas' small='Cadastro' />
+                <Form />
+                <List />
             </div>
         )
     }
